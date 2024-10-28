612753efe0c9f4519fe01b28c6d59983853b1654
Azzazzeell <146361334+Azzazzeell@users.noreply.github.com>
2024-10-28 15:41:50 +0300

    Add files via upload

---
+++ Doxygen_docx/doc/html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['example_0',['Example',['../class_example.html',1,'']]]
+];
---
+++ Doxygen_docx/doc/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['getvalue_0',['getValue',['../class_example.html#a9f1ce97cb8b34030a85f82c4a9fd68de',1,'Example']]]
+];
